9b50837eea8474be5bd2cb09c58b0d757e086d76
igabm <gabr.matias@gmail.com>
2025-08-14 01:43:13 +0200

    ci: add semantic-release pipeline

---
+++ CHANGELOG.md
@@ -0,0 +1,3 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. The format is based on [Conventional Commits](https://www.conventionalcommits.org/) and this project uses [semantic-release](https://github.com/semantic-release/semantic-release).
---
+++ release.config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  branches: ['main'],
+  repositoryUrl: 'https://github.com/igabm/n8n-nodes-tiktok',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
+    [
+      '@semantic-release/git',
+      {
+        assets: ['CHANGELOG.md', 'package.json', 'pnpm-lock.yaml'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+    '@semantic-release/github',
+  ],
+};
